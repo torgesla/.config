99da52b9cdee072ef1924cf299b6936e493646e4
Torgeir Sandnes Laurvik <torgeir.laurvik@gmail.com>
2024-08-01 23:53:35 +0200

    add precognition but disabled

--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -11,34 +11,26 @@ require('lazy').setup({
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
   -- Use `opts = {}` to force a plugin to be loaded.
-  require 'kickstart/plugins/gitsigns',
-  require 'kickstart/plugins/which-key',
-  require 'kickstart/plugins/telescope',
-  require 'kickstart/plugins/lspconfig',
-  require 'kickstart/plugins/conform',
-  require 'kickstart/plugins/cmp',
-  require 'kickstart/plugins/mini',
-  require 'kickstart/plugins/todo-comments',
-  require 'kickstart/plugins/tokyonight',
-  require 'kickstart/plugins/treesitter',
 
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
-  -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
-  --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
+  require 'kickstart/plugins/cmp',
+  require 'kickstart/plugins/conform',
+  require 'kickstart/plugins/lspconfig',
+  require 'kickstart/plugins/mini',
+  --  require 'kickstart/plugins/precognition',
+  require 'kickstart/plugins/telescope',
+  require 'kickstart/plugins/todo-comments',
+  require 'kickstart/plugins/tokyonight',
+  require 'kickstart/plugins/treesitter',
+  require 'kickstart/plugins/which-key',
+  require 'kickstart/plugins/gitsigns',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
-  --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
